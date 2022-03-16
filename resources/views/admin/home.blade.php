fd7aa5ddd5d52b25edce0bbddb9efa1a701e309b
riyad-rb <sailor.ryd@gmail.com>
2022-03-16 16:42:46 +0600

    Template added

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    
+<h1>This is Admin</h1>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
+
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/home',[HomeController::class,'redirect']);
+Route::get('/',[HomeController::class,'index']);
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
